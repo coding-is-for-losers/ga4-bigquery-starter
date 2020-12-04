881813a96323776efafc5c7329579b1f33677d1e
Johan van de Werken <johanvdw@gmail.com>
2020-12-04 20:05:23 +0000

    added possible solution for dealing with intraday tables

--- models/admin/intraday_tables_proc.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-    materialized='ephemeral'
-)}}
-
-SELECT * FROM 
-`{{ target.project }}.{{ target.schema }}.__TABLES__`
-WHERE table_id like 'events_intraday_%'
--- models/base/unique_events_and_parameters.sql
+++ models/base/unique_events_and_parameters.sql
@@ -14,7 +14,7 @@ FROM
   `{{ target.project }}.{{ target.schema }}.events_*`,
   UNNEST(event_params) AS params
 WHERE
-  _TABLE_SUFFIX BETWEEN FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('session_lookback_days') }} DAY))
+  regexp_extract(_table_suffix,'[0-9]+') BETWEEN FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('session_lookback_days') }} DAY))
   AND FORMAT_DATE("%Y%m%d", CURRENT_DATE())
 GROUP BY
   event_name,
