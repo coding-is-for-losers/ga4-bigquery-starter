ee8ca9de51835fa99daf4ee0259f45b3286b972e
Johan van de Werken <johanvdw@gmail.com>
2020-12-08 09:26:41 +0000

    Moved incremental logic to inner query of dedup_events model, with _table_suffix instead of table_date

--- models/base/dedup_events.sql
+++ models/base/dedup_events.sql
@@ -18,11 +18,13 @@ from (
     *,
     row_number() over (partition by user_pseudo_id, event_name, event_timestamp order by event_timestamp) as row
   from
-    `{{ target.project }}.{{ target.schema }}.events_*`)
+    `{{ target.project }}.{{ target.schema }}.events_*`
+    
+  {% if is_incremental() %}
+  -- Refresh only recent session data to limit query costs, unless running with --full-refresh
+	where regexp_extract(_table_suffix,'[0-9]+') BETWEEN FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('session_lookback_days') }} DAY)) AND
+  		FORMAT_DATE("%Y%m%d", CURRENT_DATE())
+{% endif %}  
+    )
 where
   row = 1
-{% if is_incremental() %}
--- Refresh only recent session data to limit query costs, unless running with --full-refresh
-	AND table_date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('session_lookback_days') }} DAY) AND
-  		CURRENT_DATE()
-{% endif %}
